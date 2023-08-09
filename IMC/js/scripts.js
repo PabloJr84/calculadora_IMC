8bb5b3e6b84edc7beb50577ddd6044f50f4d7aa7
Pablo Assis <52177225+PabloJr84@users.noreply.github.com>
2023-08-09 15:22:04 -0300

    Atualização final

--- IMC/js/scripts.js
+++ IMC/js/scripts.js
@@ -4,41 +4,41 @@ const data = [
       min: 0,
       max: 18.4,
       classification: "Menor que 18,5",
-      info : "Magreza" ,
+      info: "Magreza",
       obesity: "0",
-    } ,
+    },
     {
       min: 18.5,
       max: 24.9,
       classification: "Entre 18,5 e 24,9",
       info: "Normal",
       obesity: "0",
-    } ,
+    },
     {
       min: 25,
       max: 29.9,
       classification: "Entre 25,0 e 29,9",
-      info : "Sobrepeso" ,
+      info: "Sobrepeso",
       obesity: "I",
-    } ,
+    },
     {
       min: 30,
       max: 39.9,
       classification: "Entre 30,0 e 39,9",
-      info : "Obesidade" ,
+      info: "Obesidade",
       obesity: "II",
-    } ,
+    },
     {
       min: 40,
       max: 99,
-      classification : "Greater than 40.0" ,
-      info : "Obesidade grave" ,
+      classification: "Maior que 40,0",
+      info: "Obesidade grave",
       obesity: "III",
-    } ,
-  ] ;
+    },
+  ];
   
   // Seleção de elementos
-  const imcTabe = document.querySelector("#imc-table");
+  const imcTable = document.querySelector("#imc-table");
   
   const heightInput = document.querySelector("#height");
   const weightInput = document.querySelector("#weight");
@@ -49,11 +49,11 @@ const data = [
   const resultContainer = document.querySelector("#result-container");
   
   const imcNumber = document.querySelector("#imc-number span");
-  const  imcInfo  =  document . querySelector ( "#imc-info span" ) ;
+  const imcInfo = document.querySelector("#imc-info span");
   
   const backBtn = document.querySelector("#back-btn");
   
-  // Functions
+  // Funções
   function createTable(data) {
     data.forEach((item) => {
       const div = document.createElement("div");
@@ -63,7 +63,7 @@ const data = [
       classification.innerText = item.classification;
   
       const info = document.createElement("p");
-      info . innerText  =  item . information ;
+      info.innerText = item.info;
   
       const obesity = document.createElement("p");
       obesity.innerText = item.obesity;
@@ -73,7 +73,7 @@ const data = [
       div.appendChild(obesity);
   
       imcTable.appendChild(div);
-    } ) ;
+    });
   }
   
   function validDigits(text) {
@@ -97,20 +97,20 @@ const data = [
     resultContainer.classList.toggle("hide");
   }
   
-  // Heat
+  // Init
   createTable(data);
   
-  // events
+  // Eventos
   [heightInput, weightInput].forEach((el) => {
     el.addEventListener("input", (e) => {
       const updatedValue = validDigits(e.target.value);
   
       e.target.value = updatedValue;
-    } ) ;
-  } ) ;
+    });
+  });
   
   calcBtn.addEventListener("click", (e) => {
-    and . preventDefault ( ) ;
+    e.preventDefault();
   
     const weight = +weightInput.value.replace(",", ".");
     const height = +heightInput.value.replace(",", ".");
@@ -126,15 +126,15 @@ const data = [
       if (imc >= item.min && imc <= item.max) {
         info = item.info;
       }
-    } ) ;
+    });
   
     if (!info) return;
   
-    imcNumber . innerText  =  imc ;
-    imcInfo . innerText  =  info ;
+    imcNumber.innerText = imc;
+    imcInfo.innerText = info;
   
     switch (info) {
-      case  "Thinness" :
+      case "Magreza":
         imcNumber.classList.add("low");
         imcInfo.classList.add("low");
         break;
@@ -142,30 +142,30 @@ const data = [
         imcNumber.classList.add("good");
         imcInfo.classList.add("good");
         break;
-      case  "Overweight" :
+      case "Sobrepeso":
         imcNumber.classList.add("low");
         imcInfo.classList.add("low");
         break;
-      case  "Obesity" :
+      case "Obesidade":
         imcNumber.classList.add("medium");
         imcInfo.classList.add("medium");
         break;
-      case  "Serious obesity" :
+      case "Obesidade grave":
         imcNumber.classList.add("high");
         imcInfo.classList.add("high");
         break;
     }
   
     showOrHideResults();
-  } ) ;
+  });
   
   clearBtn.addEventListener("click", (e) => {
-    and . preventDefault ( ) ;
+    e.preventDefault();
   
     cleanInputs();
-  } ) ;
+  });
   
   backBtn.addEventListener("click", (e) => {
     cleanInputs();
     showOrHideResults();
-  } ) ;
+  });
